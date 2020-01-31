a7cd6418af71dfb9a3044ec5d18c471cb32e5014
Arsen Anay <arsenanai@gmail.com>
2020-01-31 16:42:33 +0600

    interactive piano is done

--- js/main-piano.js
+++ js/main-piano.js
@@ -7,89 +7,61 @@ var app = new Vue({
   el: '#app',
   data: {
     solution: 
-`var flag = true
-for(i = 2; i <= n - 1; i++)
-    if (n % i == 0) {
-        flag = false;
-        break;
-    }
-window.log('flag value is: '+flag)
-// Check and display alert message
-if (flag == true)
-    return true
-else
-    return false`,
+`var result = app.playAudio(event.keyCode)
+console.log(result)
+return result`,
     function: null,
     courseName: 'Javascript Learning Course',
     taskName: 'Piano',
-    taskDescription: 'Complete code above to bind piano keys with keyboard and play sound.',
-    inputFormat: 'Complete function below to play piano keys. Use keys from "A" to ";" and from "W" to "O", that should sound from notes accordingly. To make this task easier, we prepared sound files, which can be accessed by path "/assets/piano/a.mp3" files has names ["a"-"\\"].mp3',
-    testExample: 'On pressing keys in series "ADG" a.mp3, d.mp3, g.mp3 files has to be played',
+    taskDescription: 'Complete code above that listens to keyboard, you have to hook keys with piano sounds. Your working piano keys must be from "W" to "P" and from "A" to ";"',
+    inputFormat: 'Input format will be a list of KeyboardEvents. we prepared function called app.playAudio(keyCode[keyboard\'s number represantation in ASCII table]). You have to return the result of calling that function. Don\'t take to consideration when some other key is typed' ,
+    testExample: 'KeyboardEvent with "ADT" keys will be typed in series, the function app.playAudio() should be called 3 times with respective keyCode. In this example they are: 65,83,84',
     tested: false,
+    alreadyRun: false,
+    canBeRun: true,
     submitted: false,
     results:[],
     testCases:{
     	type: "functional",
+        
         call: 'onKeyPress',
         arguments:[
             {
-                name: 'key'
+                name: 'event'
             }
         ],
     	cases: [
     		{
                 beforeSubmit: true,
     			input:{
-    				type: 'number',
-    				value: 5,
+    				type: 'event',
+    				value: {keycode:'65'},
     			},
     			output:{
-    				type: 'boolean',
-    				value: true,    			
+    				type: 'string',
+    				value: 'C',    			
     			},
     		},
     		{
                 beforeSubmit: true,
-    			input:{
-    				type: 'number',
-    				value: 6,
-    			},
-    			output:{
-    				type: 'boolean',
-    				value: false,    			
-    			},
-    		},
-    		{
-                beforeSubmit: true,
-    			input:{
-    				type: 'number',
-    				value: 1000003,
-    			},
-    			output:{
-    				type: 'boolean',
-    				value: true,    			
-    			},
-    		},
-            {
-                beforeSubmit: false,
                 input:{
-                    type: 'number',
-                    value: 1000004,
+                    type: 'event',
+                    value: {keycode:'70'},
                 },
                 output:{
-                    type: 'boolean',
-                    value: false,                
+                    type: 'string',
+                    value: 'F',             
                 },
             },
             {
-                beforeSubmit: false,
+                beforeSubmit: true,
                 input:{
-                    type: 'number',
-                    value: 10007,
+                    type: 'event',
+                    value: {keycode:'186'},
                 },
                 output:{
-                    type: 'boolean',
-                    value: true,                
+                    type: 'string',
+                    value: 'E',             
                 },
             },
     	]
@@ -102,18 +74,26 @@ else
     postfix: function(){
         return `}`
     },
+    run: function(){
+        //resetting previous tests
+        this.beforeRunInitiated()
+        //window.logs=[]
+        //validation solution
+        //evaluating solution
+        if(this.testCases.type=='functional'){
+            this.function = new Function("return " + "function("+this.commaSeparated(this.testCases.arguments,"name")+"){ "+this.solution+" }")();
+        }
+        this.onRunInitiated()
+    },
     test: function(){
         //resetting previous tests
-        this.tested = false
-        this.submitted = false
-        this.results = []
+        /*this.beforeTestInitiated()
         //window.logs=[]
         //validation solution
 
         //evaluating solution
-        
         if(this.testCases.type=='functional'){
-            var func = new Function("return " + "function("+this.commaSeparated(this.testCases.arguments,"name")+"){ "+this.solution+" }")();
+            this.function = new Function("return " + "function("+this.commaSeparated(this.testCases.arguments,"name")+"){ "+this.solution+" }")();
             var result
             this.testCases.cases.forEach((cas)=>{
                 if(cas.beforeSubmit==true){
@@ -124,7 +104,9 @@ else
                 }
             })
         }
-        this.tested = true
+        this.onTestInitiated()
+        if(this.testManualStop == false)
+            this.onTestStopped()*/
     },
     commaSeparated: function(a/*array*/,attr/*field*/){
         return a.map((e)=>e[attr]).join(", ")
@@ -135,24 +117,49 @@ else
             alert('Done');
         }
     },
+    beforeRunInitiated: function(){
+        this.submitted = false
+        this.results = []
+        this.alreadyRun = true
+    },
+    onRunInitiated: function(){
+        if(this.testCases.type=='functional'){
+            window.addEventListener("keydown", this.function)
+        }
+    },
+    onRunStopped: function(){
+        if(this.testCases.type=='functional'){
+            window.removeEventListener("keydown", this.function)
+        }
+        this.alreadyRun = false
+    },
+    /*hintsOn: function(e, index) {
+      e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
+    },*/
+    playAudio: function(keyCode){
+      const note = document.querySelector(".nowplaying"),
+      audio = document.querySelector(`audio[data-key="${keyCode}"]`),
+        key = document.querySelector(`.key[data-key="${keyCode}"]`);
+      if (!key) return -1;
+      key.classList.add("playing");
+      const keyNote = key.getAttribute("data-note");
+      note.innerHTML = keyNote;
+      audio.currentTime = 0;
+      audio.play();
+      return keyNote
+    },
     removeTransition: function(e) {
       if (e.propertyName !== "transform") return;
-      this.classList.remove("playing");
+      e.target.classList.remove("playing");
     },
-    hintsOn: function(e, index) {
-      e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
-    },
-    debug: function(event){
-        console.log(event)
-    }
   },
   created(){
-    const keys = document.querySelectorAll(".key"),
+    /*const keys = document.querySelectorAll(".key"),
       note = document.querySelector(".nowplaying"),
-      hints = document.querySelectorAll(".hints");
-      //hints.forEach(this.hintsOn);
-
+      hints = document.querySelectorAll(".hints");*/
     
-    /*keys.forEach(key => key.addEventListener("transitionend", this.removeTransition));*/
   },
 })
+
+const keys = document.querySelectorAll(".key")
+keys.forEach(key => key.addEventListener("transitionend", app.removeTransition));
